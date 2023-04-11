d6309ee433542f50513161d50de273e3578007ba
Li, Jimin <jimin.li@intel.com>
2023-04-11 10:44:16 +0800

    update cmdrc.bat

--- cmdrc.bat
+++ cmdrc.bat
@@ -9,21 +9,26 @@
 @set "PROMPT=$e[33m%PROMPT%$e[0m"
 
 :: alias/doskey
+:: path
 doskey ..=cd ..
 doskey ...=cd ..\..
-doskey ls=dir
 doskey pwd=cd
 doskey home=cd %HOMEPATH%
-doskey rm=del
-doskey cat=type
-doskey ps=tasklist
-doskey which=where
+
+doskey ls=dir $*
 REM order by date
 doskey llrt="dir /OD"
-
-doskey ex=explorer $*
+doskey rm=del $*
+doskey cat=type $*
+doskey ps=tasklist
+doskey which=where $*
+REM cannot be used in "command | grep xxx"
+REM doskey grep="findstr /i"
 REM doskey source=call
+
+:: GUI apps/tools
+doskey ex=explorer $*
 doskey np="C:\x\bin_tools\green\npp.7.8.bin.x64\notepad++.exe" $*
 doskey gvim="C:\Program Files (x86)\Vim\vim90\gvim.exe" $*
 
-@echo cmdrc sourced, aliases defined, return
+@echo cmdrc in USERPROFILE\bin sourced, aliases defined, return
